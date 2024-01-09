f3e6f7c642a334549a19d7c60848b27b0e782190
Tony CSERESZNYAK <TonyCseresznyak@hotmail.com>
2024-01-09 16:16:25 +0100

    Initiation of the react project, addition of the navbar and header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+      return (
+            <div id='main'>
+                  <div className='name'>
+                        <h2>Hello je m'appelle</h2>
+                        <h1>Tony Cseresznyak</h1>
+                        <p className='details'>Je suis un développeur web junior full stack avec un penchant pour le front et une forte sensibilité à l'UX/UI design. <br/><br/> Actuellement je suis en recherche d'une alternance pour mon bac+4 Développeur web full stack avec <a className="details-link" href="https://cloud-campus.fr/developpeur-web-full-stack/"> Cloud Campus</a>.</p>
+                  </div>
+            </div>
+      )
+}
+
+export default Header;
