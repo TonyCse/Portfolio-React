73d0f095f978e91ba99987559d0badfda77c657b
Tony CSERESZNYAK <TonyCseresznyak@hotmail.com>
2024-01-10 18:15:11 +0100

    Experiences component update

--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -3,9 +3,32 @@ import React from 'react'
 function Experiences() {
       return (
             <div id='experiences'>
-               <div className='title'>
+                  <div className='title'>
                         <h2> 02. Expériences professionnelles</h2>
                         <div className='line'></div>
+                  </div>
+                  <div className='experiences-main'>
+                        <h2>Experiences professionnelles</h2>
+                        <div className='experiences-card'>
+                              <div className='experiences-date'>
+                                    Sept 2022 - Mar 2023
+                              </div>
+                              <div className='experiences-body'>
+                                    <div className='experiences-body-title'>
+                                          Stage Développeur Front-End · Habiteo · Paris
+                                    </div>
+                                    <div className='experiences-body-text'>
+                                          Développer et optimiser l'ergonomie et la navigation de 3 applications web pour une start-up spécialisée dans l'innovation immobilière 3D.
+                                    </div>
+                                    <div className='experiences-body-footer'>
+                                          <span >HTML/CSS/SCSS</span>
+                                          <span >JavaScript</span>
+                                          <span >Nodes.js</span>
+                                          <span >Vue.js</span>
+                                          <span >Vuetify</span>
+                                    </div>
+                              </div>
+                        </div>
                   </div>
             </div>
       )
