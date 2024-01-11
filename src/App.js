8dd6555497d269297d508c5d2ffe82576f068756
Tony CSERESZNYAK <TonyCseresznyak@hotmail.com>
2024-01-11 14:59:46 +0100

    Adding loader + CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
+import React, {useState, useEffect} from 'react'
+import PulseLoader from "react-spinners/PulseLoader";
 import Navbar from './Components/Navbar'
 import Header from './Components/Header'
 import Presentation from './Components/Presentation';
 import Experiences from './Components/Experiences'
 import Projets from './Components/Projets'
+import logo from './images/logo.png'
 
 function App() {
+
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+        setloading(true)
+        setTimeout(() => {
+              setloading(false)
+        }, 3000)
+  }, [])
+
   return (
     <div className="App">
-      <Navbar/>
-      <Header/>
-      <Presentation/>
-      <Experiences/>
-      <Projets/>
+      { 
+            loading ?
+            <div className='chargement'> 
+              <img src={logo} alt=''/>
+              <h2>Chargement</h2>
+              <PulseLoader
+                    color={"#5bb1ab"}
+                    loading={loading}
+                    size={30}
+              />
+            </div>
+
+        : 
+      
+        <div> 
+          <Navbar/>
+          <Header/>
+          <Presentation/>
+          <Experiences/>
+          <Projets/>
+        </div>
+      }
+
+
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import iconyoutube from '../images/youtube-projet.png'
+import icongithub from '../images/github-projet.png'
+
 
 function Header() {
       return (
@@ -7,7 +10,15 @@ function Header() {
                         <h2>Hello je m'appelle</h2>
                         <h1>Tony Cseresznyak</h1>
                         <h3>Développeur Web</h3>
-                        <p className='details'>Je suis un développeur web junior full stack avec un penchant pour le front et une forte sensibilité à l'UX/UI design. <br/><br/> Actuellement je suis en recherche d'une alternance pour mon bac+4 Développeur web full stack avec <a className="details-link" href="https://cloud-campus.fr/developpeur-web-full-stack/"> Cloud Campus</a>.</p>
+                        <p className='details'>Je suis un développeur web junior full stack avec un penchant pour le front et une forte sensibilité à l'UX/UI design. <br/><br/> Actuellement je suis en recherche d'une alternance pour mon bac+4 Développeur web full stack avec <a className="details-link" href="https://cloud-campus.fr/developpeur-web-full-stack/"> Cloud Campus</a>.</p> 
+                        <div>
+                              <a href="https://www.youtube.com/channel/UCCkt7tnefsVbFLFuNkM09Rw">
+                                    <img src={iconyoutube} alt='youtube'/> 
+                              </a>
+                              <a href="https://github.com/TonyCse">
+                                    <img src={icongithub} alt='github'/>
+                             </a>
+                        </div>
                   </div>
             </div>
       )
